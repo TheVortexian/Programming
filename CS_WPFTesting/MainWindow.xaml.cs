ab2ef04c0215301334abc638123cbc66ea8ad921
TheVortexian <treesofgold7@gmail.com>
2019-07-24 20:15:32 -0500

    updated wpf

--- CS_WPFTesting/MainWindow.xaml.cs
+++ CS_WPFTesting/MainWindow.xaml.cs
@@ -20,14 +20,56 @@ namespace CS_WPFTesting
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string trueNum;
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void Hello_Click(object sender, RoutedEventArgs e)
+        private void C1(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("hello, world!");
+            trueNum += "1";
+        }
+        private void C2(object sender, RoutedEventArgs e)
+        {
+            trueNum += "2";
+        }
+
+        private void C3(object sender, RoutedEventArgs e)
+        {
+            trueNum += "3";
+        }
+        private void C4(object sender, RoutedEventArgs e)
+        {
+            trueNum += "4";
+        }
+        private void C5(object sender, RoutedEventArgs e)
+        {
+            trueNum += "5";
+        }
+        private void C6(object sender, RoutedEventArgs e)
+        {
+            trueNum += "6";
+        }
+        private void C7(object sender, RoutedEventArgs e)
+        {
+            trueNum += "7";
+        }
+        private void C8(object sender, RoutedEventArgs e)
+        {
+            trueNum += "8";
+        }
+        private void C9(object sender, RoutedEventArgs e)
+        {
+            trueNum += "9";
+        }
+        private void C0(object sender, RoutedEventArgs e)
+        {
+            trueNum += "0";
+        }
+        private void SHOWNUM(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show(trueNum);
         }
     }
 }
