cde8ae189e3aae99c3fd0f6312a8258ffbfc9368
TheVortexian <treesofgold7@gmail.com>
2019-07-27 18:21:30 -0500

    Updated some small CSS and JS
    Working on letting the user add new "users" to HTML table via text input and not a prompt

--- Some Website/scripts/main.js
+++ Some Website/scripts/main.js
@@ -1,3 +1,15 @@
 function newUser() {
-    document.getElementById("userDropdown").style.visibility = "visible";
+    var userTable = document.getElementById("userDropdown");
+    userTable.style.visibility = "visible";
+    var rows = userTable.getElementsByTagName("tr").length;
+    
+    var newRow = userTable.insertRow(rows-1);
+    var name, pswd, email;
+    name = prompt("Username: ");
+    pswd = prompt("Password: ");
+    email = prompt("Email: ");
+
+    newRow.insertCell(0).innerHTML = name;
+    newRow.insertCell(1).innerHTML = pswd;
+    newRow.insertCell(2).innerHTML = email;
 }
