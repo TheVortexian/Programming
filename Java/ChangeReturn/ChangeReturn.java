1a624fe69cd3a8a848906fd07da5c7cef81b2301
Vortexian <treesofgold7@gmail.com>
2019-07-30 13:05:17 -0500

    made ChangeReturn a bit faster

--- Java/ChangeReturn/ChangeReturn.java
+++ Java/ChangeReturn/ChangeReturn.java
@@ -12,10 +12,14 @@ public class ChangeReturn {
       given = s.nextFloat();
 
       System.out.println(calcChange(cost, given));
+      s.close();
    }
 
 
    public static String calcChange(float c, float g) {
+      if (c < 0 || g < 0) {
+         throw new IndexOutOfBoundsException("Prices cannot be less than 0!");
+      }
       int ones = 0, fives = 0, tens = 0, twenties = 0, fifties = 0, hundreds = 0;
       int quarters = 0; // .25
       int dimes = 0; // .1
@@ -27,52 +31,28 @@ public class ChangeReturn {
 
       //iterate through change with while loops
       // might not be the most efficient method but it works
+      // you have to iterate by biggest -> smallest
       if (totalChange > 0) {
-
-        //bills
-        while (totalChange - 100 >= 0) {
-          hundreds++;
-          totalChange -= 100;
-        }
-        while (totalChange - 50 >= 0) {
-          fifties++;
-          totalChange -= 50;
-        }
-        while (totalChange - 20 >= 0) {
-          twenties++;
-          totalChange -= 20;
-        }
-        while (totalChange - 10 >= 0) {
-          tens++;
-          totalChange -= 10;
-        }
-        while (totalChange - 5 >= 0) {
-          fives++;
-          totalChange -= 5;
-        }
-        while (totalChange - 1 >= 0) {
-          ones++;
-          totalChange -= 1;
-        }
-
-        //Coins
-         while (totalChange - .25 >= 0) {
-            quarters++;
-            totalChange -= .25;
-         }
-         while (totalChange - .1 >= 0) {
-            dimes++;
-            totalChange -= .1;
-         }
-         while (totalChange - .05 >= 0) {
-            nickels++;
-            totalChange -= .05;
-         }
-         while (totalChange - .01 >= 0) {
-            pennies++;
-            totalChange -= .01;
-         }
-
+         hundreds = (int)(totalChange / 100);
+         totalChange %= 100;
+         fifties = (int)(totalChange / 50);
+         totalChange %= 50;
+         twenties = (int)(totalChange / 20);
+         totalChange %= 20;
+         tens = (int)(totalChange / 10);
+         totalChange %= 10;
+         fives = (int)(totalChange / 5);
+         totalChange %= 5;
+         ones = (int)(totalChange);
+         totalChange %= 1; // not sure if this is needed
+         quarters = (int)(totalChange / .25);
+         totalChange %= .25;
+         dimes = (int)(totalChange / .1);
+         totalChange %= .1;
+         nickels = (int)(totalChange / .05);
+         totalChange %= .05;
+         pennies = (int)Math.ceil((totalChange / .01));
+         totalChange %= .01;
          return("Total change: " + 
                String.format("%.2f",(given-cost)) + "\nHundreds: " + hundreds + "\nFifties: " + fifties + 
                "\nTwenties: " + twenties + "\nTens: " + tens + "\nFives: " + fives + "\nOnes: " + ones +
