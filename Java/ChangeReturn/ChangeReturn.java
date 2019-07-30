5f3dcc5db2f8318fa62fbec327691c9e65da5f1d
Vortexian <treesofgold7@gmail.com>
2019-07-29 19:46:28 -0500

    updated changereturn

--- Java/ChangeReturn/ChangeReturn.java
+++ Java/ChangeReturn/ChangeReturn.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class ChangeReturn {
-   
+
    public static void main(String[] args) {
       Scanner s = new Scanner(System.in);
       float cost = 0;
@@ -10,13 +10,13 @@ public class ChangeReturn {
       cost = s.nextFloat();
       System.out.println("Enter money given: ");
       given = s.nextFloat();
-      
+
       System.out.println(calcChange(cost, given));
    }
-   
-   
+
+
    public static String calcChange(float c, float g) {
-      
+      int ones = 0, fives = 0, tens = 0, twenties = 0, fifties = 0, hundreds = 0;
       int quarters = 0; // .25
       int dimes = 0; // .1
       int nickels = 0; // .05
@@ -24,9 +24,38 @@ public class ChangeReturn {
       float cost = c;
       float given = g;
       float totalChange = (given-cost);
-      
+
+      //iterate through change with while loops
+      // might not be the most efficient method but it works
       if (totalChange > 0) {
-         
+
+        //bills
+        while (totalChange - 100 >= 0) {
+          hundreds++;
+          totalChange -= 100;
+        }
+        while (totalChange - 50 >= 0) {
+          fifties++;
+          totalChange -= 50;
+        }
+        while (totalChange - 20 >= 0) {
+          twenties++;
+          totalChange -= 20;
+        }
+        while (totalChange - 10 >= 0) {
+          tens++;
+          totalChange -= 10;
+        }
+        while (totalChange - 5 >= 0) {
+          fives++;
+          totalChange -= 5;
+        }
+        while (totalChange - 1 >= 0) {
+          ones++;
+          totalChange -= 1;
+        }
+
+        //Coins
          while (totalChange - .25 >= 0) {
             quarters++;
             totalChange -= .25;
@@ -43,7 +72,7 @@ public class ChangeReturn {
             pennies++;
             totalChange -= .01;
          }
-         
+
          return("Total change: " + String.format("%.2f",(given-cost)) + "\nQuarters: " + quarters + "\nDimes: " + dimes + "\nNickels: " + nickels + "\nPennies: " + pennies);
       }
       if (totalChange == 0) {
