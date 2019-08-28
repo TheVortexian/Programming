320d6c1495dd74d5e436592dc9cd57dda70c75c2
Vortexian <treesofgold7@gmail.com>
2019-08-28 17:42:47 -0500

    thing

---
+++ Java/CustomStack/Driver.java
@@ -0,0 +1,11 @@
+/**
+* Connor L.
+* the driver code for my custom Stack
+*/
+public class Driver {
+
+   public static void main(String[] args) {
+      CustomStack<Double> l = new CustomStack<Double>();
+   }
+
+}
