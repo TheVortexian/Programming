501ba8c8b58eb9c6dbb6f41ee5996e9b63b13d9d
TheVortexian <treesofgold7@gmail.com>
2020-01-13 18:23:28 -0600

    print

---
+++ C++/Basics/Iostream/print.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+   
+   std::cout << "Hello, World!" << std::endl;
+   std::cout << "Hello, " << " world!" << std::endl;
+   
+   int x = 4;
+   std::cout << x << std::endl;
+
+   return 0;
+}
