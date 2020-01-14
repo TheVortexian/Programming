137a23b69fa84ab1f1dbc6b3ac8af4913605f467
TheVortexian <treesofgold7@gmail.com>
2020-01-13 18:34:39 -0600

    Create literals.cpp

---
+++ C++/Basics/literals and operators/literals.cpp
@@ -0,0 +1,8 @@
+
+int main() {
+   char myMessage[] = "Hello, World!"; // technically a string
+   int myInteger = 1050; // an integer literal
+   double myDouble = 105.00123; // a double literal
+   float myFloat = 100.0; // a float literal, et al
+   return 0;
+}
