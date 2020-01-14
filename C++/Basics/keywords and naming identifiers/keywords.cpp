2bc8147285bdd8aead4f8d6f96a2680b346e0afb
TheVortexian <treesofgold7@gmail.com>
2020-01-13 18:32:00 -0600

    Update keywords.cpp

--- C++/Basics/keywords and naming identifiers/keywords.cpp
+++ C++/Basics/keywords and naming identifiers/keywords.cpp
@@ -6,5 +6,13 @@ https://www.learncpp.com/cpp-tutorial/keywords-and-naming-identifiers/
 using namespace std;
 
 int main() {
+   int value; // correct
+ 
+   int Value; // incorrect (should start with lower case letter)
+   int VALUE; // incorrect (should start with lower case letter)
+   int VaLuE; // incorrect (see your psychiatrist) ;)
+   
+   // comments are useful too!
+   int data = 1001010; // this variable holds a random number!
    return 0;
 }
