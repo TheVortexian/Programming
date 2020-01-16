6644866710f451511f10e5d58db18a2072f72d6d
TheVortexian <treesofgold7@gmail.com>
2020-01-15 19:25:08 -0600

    major change
    combined all functions to handle both registers AND values
    
    can parse files properly

--- Java/Assembly/ASM_File.java
+++ Java/Assembly/ASM_File.java
@@ -8,26 +8,23 @@ public class ASM_File {
    protected File file;
    protected HashMap<String, Integer> genRegs; // generic registers
    protected HashMap<String, Double> xmmRegs; // xmm registers
-   protected HashMap<Integer, String> lines; // holds lines by line num? gotta keep track of lines
-                                             // I dont want to explicitly use -IP so not sure what to do
+   protected HashMap<Integer, String> programFile; // holds lines
    private int lineCount = 0;
    public ASM_File(String fileName) {
       this.fileName = fileName;
       this.file = new File(fileName);
       this.genRegs = new HashMap<String, Integer>();
       this.xmmRegs = new HashMap<String, Double>();
-      this.lines = new HashMap<Integer, String>();
+      this.programFile = new HashMap<Integer, String>();
       try {
          Scanner s = new Scanner(this.file);
-         int line = 1;
          while (s.hasNextLine()) {
-            this.lines.put(line, s.nextLine());
-            line++;
-            lineCount++;
+            programFile.put(this.lineCount++, s.nextLine());
          }
       } catch (FileNotFoundException e) {
          System.out.println("Could not read file!");
       }
+      System.out.println(programFile);
    }
    
    // puts a generic register / value
@@ -88,175 +85,278 @@ public class ASM_File {
       return ret;
    }
    
-   // mov reg, reg
-   protected void mov(String reg1, String reg2) {
+   // mov reg, reg (or value)
+   protected void mov(String reg1, Object toMove) {
+      // reg, reg
+      if (toMove instanceof String) {
+         String reg2 = String.valueOf(toMove);
+         if (!regExists(reg1)) throw new NullPointerException();
+         if (!regExists(reg2)) throw new NullPointerException();
+         else {
+            if (reg2.contains("r") && reg1.contains("r")) {
+               genRegs.replace(reg1, genRegs.get(reg2));
+            } else if (reg1.contains("xmm") && reg2.contains("xmm")) {
+               xmmRegs.replace(reg1, xmmRegs.get(reg2));
+            } else {
+               System.out.println("Registers do not match!");
+               throw new InputMismatchException();
+            } 
+         }
+      }
       
-      if (!regExists(reg1)) {
-         if (reg1.contains("r")) genRegs.put(reg1, 0);
-         else if (reg1.contains("xmm")) xmmRegs.put(reg1, 0.0);
-      }
-      if (!regExists(reg2)) {
-         if (reg2.contains("r")) genRegs.put(reg2, 0);
-         else if (reg2.contains("xmm")) xmmRegs.put(reg2, 0.0);
+      // reg, val
+      if (toMove instanceof Integer) {
+         int tempMov = 0;
+         try {
+            tempMov = Integer.parseInt(String.valueOf(toMove));
+         } catch (Exception e) {
+            System.out.println("Couldn't parse move value!");
+            throw e;
+         }
+         if (reg1.contains("r")) {
+            if (regExists(reg1)) {
+               genRegs.replace(reg1, tempMov);
+            } else if (!regExists(reg1)) {
+               genRegs.put(reg1, tempMov);
+            }
+         } else { System.out.println("Can't move integer into a non-integer register!"); throw new InputMismatchException(); }
       }
       
-      if ((reg1.contains("xmm") && !reg2.contains("xmm")) || (reg1.contains("r") && !reg2.contains("r"))) {
-         throw new InputMismatchException();
+      else if (toMove instanceof Double) {
+         double tempMov = 0;
+         try {
+            tempMov = Double.parseDouble(String.valueOf(toMove));
+         } catch (Exception e) {
+            System.out.println("Couldn't parse move value!");
+            throw e;
+         }
+         if (reg1.contains("xmm")) {
+            if (regExists(reg1)) {
+               xmmRegs.replace(reg1, tempMov);
+            } else if (!regExists(reg1)) {
+               xmmRegs.put(reg1, tempMov);
+            }
+         } else { System.out.println("Can't move floating point into non-floating point register!"); throw new InputMismatchException(); }
       }
-      else if (reg1.contains("xmm") && reg2.contains("xmm")) {
-         if (regExists(reg1) && regExists(reg2)) {
-            xmmRegs.replace(reg1, xmmRegs.get(reg2));
-         } else {
+      else {
+         System.out.println("Couldn't identify registers or values!");
+         throw new NullPointerException();
+      }
+   } 
+   
+   // add reg, reg (or value)
+   protected void add(String reg1, Object toAdd) {
+      if (!(toAdd instanceof String)) {
+         if (!regExists(reg1)) {
+            System.out.println("Cannot add to nonexistent register!");
             throw new NullPointerException();
          }
+         else if (regExists(reg1)) {
+            if (toAdd instanceof Integer && reg1.contains("r")) {
+               try {
+                  genRegs.replace(reg1, genRegs.get(reg1) + Integer.parseInt(String.valueOf(toAdd)));
+               } catch (Exception e) { System.out.println("Couldn't parse add value!"); throw e; }
+            } else if (toAdd instanceof Double && reg1.contains("xmm")) {
+               try {
+                  xmmRegs.put(reg1, xmmRegs.get(reg1) + Double.parseDouble(String.valueOf(toAdd)));
+               } catch (Exception e) { System.out.println("Couldn't parse add value!"); throw e; }
+            }
+         }
       }
-      else if (reg1.contains("r") && reg2.contains("r")) {
-         if (regExists(reg1) && regExists(reg2)) {
-            genRegs.replace(reg1, genRegs.get(reg2));
+      else if (toAdd instanceof String) {
+         String reg2 = String.valueOf(toAdd);
+         if (reg1.contains("r") && reg2.contains("r")) {
+            if (regExists(reg1) && regExists(reg2)) {
+               genRegs.replace(reg1, genRegs.get(reg1) + genRegs.get(reg2));
+            } else {
+               System.out.println("Cannot add nonexistent registers!");
+               throw new NullPointerException();
+            }
+         } else if (reg1.contains("xmm") && reg2.contains("xmm")) {
+            if (regExists(reg1) && regExists(reg2)) {
+               xmmRegs.replace(reg1, xmmRegs.get(reg1) + xmmRegs.get(reg2));
+            } else {
+               System.out.println("Cannot add nonexistent registers!");
+               throw new NullPointerException();
+            }
          } else {
+            System.out.println("Couldn't identify registers!");
+            throw new InputMismatchException();
+         }
+      } else {
+         System.out.println("Couldn't parse anything; this is probably a bug, please look at the add() function in ASM_File.java!");
+         throw new NullPointerException();
+      }
+   }
+   
+   
+   // sub reg, reg (or value)
+   protected void sub(String reg1, Object toAdd) {
+      if (!(toAdd instanceof String)) {
+         if (!regExists(reg1)) {
+            System.out.println("Cannot sub to nonexistent register!");
             throw new NullPointerException();
          }
-      }
-   }
-   
-   // mov reg, val
-   protected void mov(String reg, Object val) {
-      if (regExists(reg)) {
-         if (reg.contains("r") && val instanceof Integer) {
-            genRegs.replace(reg, Integer.parseInt(String.valueOf(val)));
-         } else if (reg.contains("xmm") && val instanceof Double) {
-            xmmRegs.replace(reg, Double.parseDouble(String.valueOf(val)));
-         } else {
-            throw new IllegalArgumentException();
-         }
-      } else if (!regExists(reg)) {
-         if (reg.contains("r") && val instanceof Integer) {
-            genRegs.put(reg, Integer.parseInt(String.valueOf(val)));
-         } else if (reg.contains("xmm") && val instanceof Double) {
-            xmmRegs.put(reg, Double.parseDouble(String.valueOf(val)));
-         } else {
-            throw new IllegalArgumentException();
+         else if (regExists(reg1)) {
+            if (toAdd instanceof Integer && reg1.contains("r")) {
+               try {
+                  genRegs.replace(reg1, genRegs.get(reg1) - Integer.parseInt(String.valueOf(toAdd)));
+               } catch (Exception e) { System.out.println("Couldn't parse sub value!"); throw e; }
+            } else if (toAdd instanceof Double && reg1.contains("xmm")) {
+               try {
+                  xmmRegs.put(reg1, xmmRegs.get(reg1) - Double.parseDouble(String.valueOf(toAdd)));
+               } catch (Exception e) { System.out.println("Couldn't parse sub value!"); throw e; }
+            }
          }
       }
-   }
-   
-   // add reg, reg
-   protected void add(String reg1, String reg2) {
-      if (regExists(reg1) && regExists(reg2)) {
+      else if (toAdd instanceof String) {
+         String reg2 = String.valueOf(toAdd);
          if (reg1.contains("r") && reg2.contains("r")) {
-            genRegs.replace(reg1, genRegs.get(reg1) + genRegs.get(reg2));
+            if (regExists(reg1) && regExists(reg2)) {
+               genRegs.replace(reg1, genRegs.get(reg1) - genRegs.get(reg2));
+            } else {
+               System.out.println("Cannot sub nonexistent registers!");
+               throw new NullPointerException();
+            }
          } else if (reg1.contains("xmm") && reg2.contains("xmm")) {
-            xmmRegs.replace(reg1, xmmRegs.get(reg1) + xmmRegs.get(reg2));
+            if (regExists(reg1) && regExists(reg2)) {
+               xmmRegs.replace(reg1, xmmRegs.get(reg1) - xmmRegs.get(reg2));
+            } else {
+               System.out.println("Cannot sub nonexistent registers!");
+               throw new NullPointerException();
+            }
          } else {
+            System.out.println("Couldn't identify registers!");
             throw new InputMismatchException();
          }
       } else {
-         throw new IllegalArgumentException();
+         System.out.println("Couldn't parse anything; this is probably a bug, please look at the sub() function in ASM_File.java!");
+         throw new NullPointerException();
       }
    }
    
-   // add reg, val
-   protected void add(String reg, Object val) {
-      if (regExists(reg)) {
-         if (reg.contains("r") && val instanceof Integer) {
-            genRegs.replace(reg, genRegs.get(reg) + Integer.parseInt(String.valueOf(val)));
-         } else if (reg.contains("xmm") && val instanceof Double) {
-            xmmRegs.replace(reg, xmmRegs.get(reg) + Double.parseDouble(String.valueOf(val)));
-         } else {
-            throw new InputMismatchException();
+   // mul reg, reg (or value)
+   protected void mul(String reg1, Object toAdd) {
+      if (!(toAdd instanceof String)) {
+         if (!regExists(reg1)) {
+            System.out.println("Cannot mul to nonexistent register!");
+            throw new NullPointerException();
          }
-      } else {
-         throw new IllegalArgumentException();
+         else if (regExists(reg1)) {
+            if (toAdd instanceof Integer && reg1.contains("r")) {
+               try {
+                  genRegs.replace(reg1, genRegs.get(reg1) * Integer.parseInt(String.valueOf(toAdd)));
+               } catch (Exception e) { System.out.println("Couldn't parse mul value!"); throw e; }
+            } else if (toAdd instanceof Double && reg1.contains("xmm")) {
+               try {
+                  xmmRegs.put(reg1, xmmRegs.get(reg1) * Double.parseDouble(String.valueOf(toAdd)));
+               } catch (Exception e) { System.out.println("Couldn't parse mul value!"); throw e; }
+            }
+         }
       }
-   }
-   
-   // sub reg, reg
-   protected void sub(String reg1, String reg2) {
-      if (regExists(reg1) && regExists(reg2)) {
+      else if (toAdd instanceof String) {
+         String reg2 = String.valueOf(toAdd);
          if (reg1.contains("r") && reg2.contains("r")) {
-            genRegs.replace(reg1, genRegs.get(reg1) - genRegs.get(reg2));
+            if (regExists(reg1) && regExists(reg2)) {
+               genRegs.replace(reg1, genRegs.get(reg1) * genRegs.get(reg2));
+            } else {
+               System.out.println("Cannot mul nonexistent registers!");
+               throw new NullPointerException();
+            }
          } else if (reg1.contains("xmm") && reg2.contains("xmm")) {
-            xmmRegs.replace(reg1, xmmRegs.get(reg1) - xmmRegs.get(reg2));
+            if (regExists(reg1) && regExists(reg2)) {
+               xmmRegs.replace(reg1, xmmRegs.get(reg1) * xmmRegs.get(reg2));
+            } else {
+               System.out.println("Cannot mul nonexistent registers!");
+               throw new NullPointerException();
+            }
          } else {
+            System.out.println("Couldn't identify registers!");
             throw new InputMismatchException();
          }
       } else {
-         throw new IllegalArgumentException();
+         System.out.println("Couldn't parse anything; this is probably a bug, please look at the mul() function in ASM_File.java!");
+         throw new NullPointerException();
       }
    }
    
-   // add sub, val
-   protected void sub(String reg, Object val) {
-      if (regExists(reg)) {
-         if (reg.contains("r") && val instanceof Integer) {
-            genRegs.replace(reg, genRegs.get(reg) - Integer.parseInt(String.valueOf(val)));
-         } else if (reg.contains("xmm") && val instanceof Double) {
-            xmmRegs.replace(reg, xmmRegs.get(reg) - Double.parseDouble(String.valueOf(val)));
-         } else {
-            throw new InputMismatchException();
+   // div reg, reg (or value)
+   protected void div(String reg1, Object toAdd) {
+      if (!(toAdd instanceof String)) {
+         if (!regExists(reg1)) {
+            System.out.println("Cannot div to nonexistent register!");
+            throw new NullPointerException();
          }
-      } else {
-         throw new IllegalArgumentException();
+         else if (regExists(reg1)) {
+            if (toAdd instanceof Integer && reg1.contains("r")) {
+               try {
+                  genRegs.replace(reg1, genRegs.get(reg1) / Integer.parseInt(String.valueOf(toAdd)));
+               } catch (Exception e) { System.out.println("Couldn't parse div value!"); throw e; }
+            } else if (toAdd instanceof Double && reg1.contains("xmm")) {
+               try {
+                  xmmRegs.put(reg1, xmmRegs.get(reg1) / Double.parseDouble(String.valueOf(toAdd)));
+               } catch (Exception e) { System.out.println("Couldn't parse div value!"); throw e; }
+            }
+         }
       }
-   }
-   
-   // mul reg, reg
-   protected void mul(String reg1, String reg2) {
-      if (regExists(reg1) && regExists(reg2)) {
+      else if (toAdd instanceof String) {
+         String reg2 = String.valueOf(toAdd);
          if (reg1.contains("r") && reg2.contains("r")) {
-            genRegs.replace(reg1, genRegs.get(reg1) * genRegs.get(reg2));
+            if (regExists(reg1) && regExists(reg2)) {
+               genRegs.replace(reg1, genRegs.get(reg1) / genRegs.get(reg2));
+            } else {
+               System.out.println("Cannot div nonexistent registers!");
+               throw new NullPointerException();
+            }
          } else if (reg1.contains("xmm") && reg2.contains("xmm")) {
-            xmmRegs.replace(reg1, xmmRegs.get(reg1) * xmmRegs.get(reg2));
+            if (regExists(reg1) && regExists(reg2)) {
+               xmmRegs.replace(reg1, xmmRegs.get(reg1) / xmmRegs.get(reg2));
+            } else {
+               System.out.println("Cannot div nonexistent registers!");
+               throw new NullPointerException();
+            }
          } else {
+            System.out.println("Couldn't identify registers!");
             throw new InputMismatchException();
          }
       } else {
-         throw new IllegalArgumentException();
+         System.out.println("Couldn't parse anything; this is probably a bug, please look at the div() function in ASM_File.java!");
+         throw new NullPointerException();
       }
    }
    
-   // mul reg, val
-   protected void mul(String reg, Object val) {
-      if (regExists(reg)) {
-         if (reg.contains("r") && val instanceof Integer) {
-            genRegs.replace(reg, genRegs.get(reg) * Integer.parseInt(String.valueOf(val)));
-         } else if (reg.contains("xmm") && val instanceof Double) {
-            xmmRegs.replace(reg, xmmRegs.get(reg) * Double.parseDouble(String.valueOf(val)));
+   // the "parser" for the program file
+   protected void parse() {
+      for (String s : programFile.values()) {
+         String[] line = s.replace(",", "").split(" ");
+         String operator = line[0];
+         String register = line[1];
+         if (!Character.isLetter(line[2].charAt(0))) {
+            if (line[2].contains(".")) {
+               weirdParse(operator, register, Double.parseDouble(line[2]));
+            } else if (!line[2].contains(".")) {
+               weirdParse(operator, register, Integer.parseInt(line[2]));
+            }
          } else {
-            throw new InputMismatchException();
+            weirdParse(operator, register, line[2]);
          }
-      } else {
-         throw new IllegalArgumentException();
       }
    }
    
-   // div reg, reg
-   protected void div(String reg1, String reg2) {
-      if (regExists(reg1) && regExists(reg2)) {
-         if (reg1.contains("r") && reg2.contains("r")) {
-            genRegs.replace(reg1, genRegs.get(reg1) / genRegs.get(reg2));
-         } else if (reg1.contains("xmm") && reg2.contains("xmm")) {
-            xmmRegs.replace(reg1, xmmRegs.get(reg1) / xmmRegs.get(reg2));
-         } else {
-            throw new InputMismatchException();
-         }
+   private void weirdParse(String operator, String register, Object param) {
+      if (operator.equals("mov")) {
+         this.mov(register, param);
+      } else if (operator.equals("add")) {
+         this.add(register, param);
+      } else if (operator.equals("sub")) {
+         this.sub(register, param);
+      } else if (operator.equals("div")) {
+         this.div(register, param);
+      } else if (operator.equals("mul")) {
+         this.mul(register, param);
       } else {
-         throw new IllegalArgumentException();
-      }
-   }
-   
-   // div reg, val
-   protected void div(String reg, Object val) {
-      if (regExists(reg)) {
-         if (reg.contains("r") && val instanceof Integer) {
-            genRegs.replace(reg, genRegs.get(reg) / Integer.parseInt(String.valueOf(val)));
-         } else if (reg.contains("xmm") && val instanceof Double) {
-            xmmRegs.replace(reg, xmmRegs.get(reg) / Double.parseDouble(String.valueOf(val)));
-         } else {
-            throw new InputMismatchException();
-         }
-      } else {
-         throw new IllegalArgumentException();
+         System.out.println("Unknown operand!");
+         throw new NullPointerException();
       }
    }
 }
--- Java/Assembly/ASM_Main.java
+++ Java/Assembly/ASM_Main.java
@@ -8,6 +8,8 @@ public class ASM_Main {
       System.out.println("Enter file name: ");
       
       ASM_File f = new ASM_File(s.nextLine());
+      f.parse();
+      System.out.println(f.getXmmReg("xmm0"));
    }
 
 }
